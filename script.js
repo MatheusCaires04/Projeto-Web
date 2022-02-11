7d1326edd1a43169ea6e3fc523e56bb642d00867
MatheusCaires04 <matheus-bini@hotmail.com>
2022-02-11 10:12:45 -0300

    :package: calculo de orcamento

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>  
     document.querySelector(".container").classList.toggle("show--menu")
 )
+
+document.querySelector("#qtde").addEventListener("change", updatePrice)
+document.querySelector("#js").addEventListener("change", updatePrice)
+document.querySelector("#layout--sim").addEventListener("change", updatePrice)
+document.querySelector("#layout--nao").addEventListener("change", updatePrice)
+document.querySelector("#prazo").addEventListener("change", () => {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    updatePrice()
+})
+
+function updatePrice() {
+    const qtde = document.querySelector("#qtde").value
+    const haveJS = document.querySelector("#js").checked
+    const includeLayout = document.querySelector("#layout--sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+    let price = qtde * 100
+
+    if (haveJS) price *= 1.1
+    if (includeLayout) price += 500
+
+    let urgencyRate = 1 - prazo*0.1
+
+    price *= 1 + urgencyRate
+    
+    document.querySelector("#price").innerHTML = `R$ ${price.toFixed(2)}` 
+}
+
